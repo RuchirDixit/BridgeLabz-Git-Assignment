54e0b1e8f36f8a2c386151e8be7ca9dda0696008
Ruchir Dixit <ruchirtd96@gmail.com>
2020-07-27 21:58:02 +0530

    Added random attendace check feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
-echo "Welcome to Employee Wage Computation Program on master branch!"
+
+isPresent=1
+
+empCheck=$(( RANDOM%2 ))
+if [[ $empCheck -eq $isPresent ]]
+then
+	echo "Present"
+else
+	echo "Absent"
+fi
