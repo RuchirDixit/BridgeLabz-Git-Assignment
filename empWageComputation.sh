510926baa833aa6fa4e2dca0fcd0ee6a6c38aab8
Ruchir Dixit <ruchirtd96@gmail.com>
2020-07-27 22:24:59 +0530

    Used case for part time and full time employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,20 +1,17 @@
 #!/bin/bash -x
 
-isPresent=1
 isPartTime=2
 isFullTime=1
-empHrs=8
 empWagePerHour=20
-empHrsPartTime=4
 
-empCheck=$(( RANDOM%2 ))
-if [[ $empCheck -eq $isPresent ]]
-then
-	echo "Present"
-	dailyWage=$(( $empHrs*$empWagePerHour ))
-	echo "Daily Wage Full Time: "$dailyWage
-	dailyWagePT=$(( $empHrsPartTime*$empWagePerHour ))
-	echo "Daily Wage Part Time: "$dailyWagePT
-else
-	echo "Absent"
-fi
+empCheck=$(( RANDOM%3 ))
+case $empCheck in
+	$isFullTime)
+		empHrs=8;;
+	$isPartTime)
+		empHrs=4;;
+	*)
+		empHrs=0;;
+esac
+
+empWage=$(( $empHrs*$empWagePerHour ))
