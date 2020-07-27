abe45373d501c53fcc312c1a1d20763e91be38b7
Ruchir Dixit <ruchirtd96@gmail.com>
2020-07-27 22:18:01 +0530

    Added part time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -x
 
 isPresent=1
-
+isPartTime=2
+isFullTime=1
 empHrs=8
 empWagePerHour=20
-
+empHrsPartTime=4
 
 empCheck=$(( RANDOM%2 ))
 if [[ $empCheck -eq $isPresent ]]
 then
 	echo "Present"
 	dailyWage=$(( $empHrs*$empWagePerHour ))
-	echo "Daily Wage: "$dailyWage
+	echo "Daily Wage Full Time: "$dailyWage
+	dailyWagePT=$(( $empHrsPartTime*$empWagePerHour ))
+	echo "Daily Wage Part Time: "$dailyWagePT
 else
 	echo "Absent"
 fi
