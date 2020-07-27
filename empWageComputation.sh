36579f09304e616b5e7bc05136d61024f116d01e
Ruchir Dixit <ruchirtd96@gmail.com>
2020-07-27 22:30:50 +0530

    Calculated wage for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,15 +3,19 @@
 isPartTime=2
 isFullTime=1
 empWagePerHour=20
+totalEmpHrs=0
 
-empCheck=$(( RANDOM%3 ))
-case $empCheck in
-	$isFullTime)
-		empHrs=8;;
-	$isPartTime)
-		empHrs=4;;
-	*)
-		empHrs=0;;
-esac
-
-empWage=$(( $empHrs*$empWagePerHour ))
+for (( day=0;day<20;day++ ))
+do
+	empCheck=$(( RANDOM%3 ))
+	case $empCheck in
+		$isFullTime)
+			empHrs=8;;
+		$isPartTime)
+			empHrs=4;;
+		*)
+			empHrs=0;;
+	esac
+	totalEmpHrs=$(( $totalEmpHrs+$empHrs ))
+done
+empWagePerMonth=$(( $totalEmpHrs*$empWagePerHour ))
