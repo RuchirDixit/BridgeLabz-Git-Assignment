4721d2e4cc383a62ffab5fdee3a3ce73cb2f11d1
Ruchir Dixit <ruchirtd96@gmail.com>
2020-07-27 22:09:09 +0530

    Calculated daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,10 +2,16 @@
 
 isPresent=1
 
+empHrs=8
+empWagePerHour=20
+
+
 empCheck=$(( RANDOM%2 ))
 if [[ $empCheck -eq $isPresent ]]
 then
 	echo "Present"
+	dailyWage=$(( $empHrs*$empWagePerHour ))
+	echo "Daily Wage: "$dailyWage
 else
 	echo "Absent"
 fi
