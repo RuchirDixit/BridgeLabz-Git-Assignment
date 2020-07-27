4c2e64926b2a72ff44f519ece2579efae9f684b8
Ruchir Dixit <ruchirtd96@gmail.com>
2020-07-27 22:38:35 +0530

    used while condition for employee hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,24 @@
 #!/bin/bash -x
 
-isPartTime=2
-isFullTime=1
-empWagePerHour=20
+IS_PART_TIME=2
+IS_FULL_TIME=1
+EMP_WAGE_PR_HOUR=20
+MAX_WORKDAYS=20
+MAX_WORK_HRS=10
 totalEmpHrs=0
+totalWorkingDays=0
 
-for (( day=0;day<20;day++ ))
+while [[ $totalWorkingDays -lt $MAX_WORKDAYS && $totalEmpHrs -lt $MAX_WORK_HRS ]]
 do
 	empCheck=$(( RANDOM%3 ))
 	case $empCheck in
-		$isFullTime)
+		$IS_FULL_TIME)
 			empHrs=8;;
-		$isPartTime)
+		$IS_PART_TIME)
 			empHrs=4;;
 		*)
 			empHrs=0;;
 	esac
 	totalEmpHrs=$(( $totalEmpHrs+$empHrs ))
 done
-empWagePerMonth=$(( $totalEmpHrs*$empWagePerHour ))
+empWagePerMonth=$(( $totalEmpHrs*$EMP_WAGE_PR_HOUR ))
